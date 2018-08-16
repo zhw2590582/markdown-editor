139f145de4beef27679d4fff79c1b3f262c8523f
Harvey Zack <laozhaochaguan@hotmail.com>
2018-08-16 22:54:38 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const webpackConfig = {
   output: {
     path: __dirname + "/",
     filename: "static/js/[name]-[hash].js",
-    publicPath: "/"
+    publicPath: "/markdown-editor/"
   },
   module: {
     rules: [{
