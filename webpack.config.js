42ae73c24de92bfbca117d25aa1b572f243757c9
Harvey Zack <laozhaochaguan@gmail.com>
2018-08-27 11:26:34 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const FileManagerPlugin = require("filemanager-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const autoprefixer = require("autoprefixer");
 const isProd = process.env.NODE_ENV === "production";
 
@@ -71,6 +73,16 @@ const webpackConfig = {
 
 if (isProd) {
   webpackConfig.plugins.push(
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false
+        }
+      }
+    }),
+    new OptimizeCssAssetsPlugin({
+      // 
+    }),
     new FileManagerPlugin({
       onStart: {
         delete: ["./static", "./index.html"]
