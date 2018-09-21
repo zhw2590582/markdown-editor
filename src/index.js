70d4deda31a5ab02debabc719cfa509fb9f71129
Harvey Zack <laozhaochaguan@hotmail.com>
2018-09-22 00:07:19 +0800

    update

--- src/index.js
+++ src/index.js
@@ -9,20 +9,40 @@ import "tui-editor/dist/tui-editor-contents.css";
 import "tui-color-picker/dist/tui-color-picker.css";
 import "highlight.js/styles/github.css";
 
+const storageKey = "__editor__";
+
+function getStorage(name) {
+  const storage = JSON.parse(window.localStorage.getItem(storageKey)) || {}
+  return name ? storage[name] : storage;
+}
+
+function setStorage(name, value) {
+  const oldStorage = getStorage();
+  const newStorage = Object.assign({}, oldStorage, {
+    [name]: value
+  });
+  window.localStorage.setItem(storageKey, JSON.stringify(newStorage));
+  return newStorage;
+}
+
+function clenaStorage() {
+  window.localStorage.removeItem(storageKey);
+}
+
 const height = new URL(window.location.href).searchParams.get("height") || document.body.clientHeight || 800;
 const editor = new Editor({
   el: document.querySelector("#editSection"),
   viewer: true,
   initialEditType: "markdown",
-  initialValue: window.localStorage.getItem("markdown"),
+  initialValue: getStorage("markdown"),
   previewStyle: "vertical",
   exts: ["scrollSync", "colorSyntax"],
   usageStatistics: false,
   height: height + "px",
   events: {
-    change: function saveStorage() {
+    change: function() {
       const markdown = editor.getMarkdown();
-      window.localStorage.setItem("markdown", markdown);
+      setStorage("markdown", markdown);
     }
   }
 });
